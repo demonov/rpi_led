b9ec889efc539a4e267accf6340f0466c4600934
Andrew Demonov <demonov@live.com>
2020-12-02 17:31:57 -0500

    Attempt to analyze

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,51 @@
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
 
 fn main() {
+    spi_rppal();
+
+    //rs_ws281x_out(18);
+
+    //rs_ws281x_out(10);
+}
+
+fn rs_ws281x_out(pin: i32) {
+    use rs_ws281x::{ControllerBuilder, ChannelBuilder, StripType};
     println!("Started");
 
-    let mut spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 3 * 1000 * 1000, Mode::Mode0).unwrap();
+    let mut controller = ControllerBuilder::new()
+        .channel(
+            0,
+            ChannelBuilder::new()
+                .pin(pin)
+                .count(300)
+                .strip_type(StripType::Ws2812)
+                .brightness(255)
+                .build(),
+        )
+        .build()
+        .unwrap();
 
+    let led = controller.leds_mut(0);
+    for i in 0..300 {
+        let i = i as usize;
+        if i % 4 < 2 {
+            led[i] = [216, 216, 216, 0];
+        } else {
+            led[i] = [12, 0, 179, 0];
+        }
+    }
 
+    controller.render().unwrap();
+    controller.wait().unwrap();
+
+    println!("Done.");
+
+}
+
+fn spi_rppal(){
+    println!("Started");
+
+    let mut spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 800 * 1000, Mode::Mode0).unwrap();
 
     let colors = [
         ColorRGB(255, 0, 0),
@@ -33,7 +73,6 @@ fn main() {
         .collect::<Vec<u8>>();
 
     spi.write(&output).unwrap();
-
 
     println!("Done.");
 }
